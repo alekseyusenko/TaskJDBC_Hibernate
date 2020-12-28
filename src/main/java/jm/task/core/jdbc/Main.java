889f8f2d92a60a680f83e8baada748978cc4226c
alekseyusenko <69609627+alekseyusenko@users.noreply.github.com>
2020-12-28 16:32:30 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,5 +3,17 @@ package jm.task.core.jdbc;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Ivan", "Petrov", (byte)40);
+        userServiceImpl.saveUser("Oleg", "Ivanov", (byte)22);
+        userServiceImpl.saveUser("Alexander", "Sidorov", (byte)56);
+        userServiceImpl.saveUser("Nikolay", "Tarasov", (byte)33);
+        List<User> users = userServiceImpl.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        } //можно и стримом, но у темплейта версия 1.8, поэтому пускай будет так, вдруг чего сломается
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
