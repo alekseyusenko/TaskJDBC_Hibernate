3fe0105685f5b51eb3adb62f8485dcc69a8e993a
alekseyusenko <69609627+alekseyusenko@users.noreply.github.com>
2020-12-28 16:28:09 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,4 +2,20 @@ package jm.task.core.jdbc.util;
 
 public class Util {
     // реализуйте настройку соеденения с БД
+       private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "12345";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection error");
+        }
+        return connection;
+    }    
 }
